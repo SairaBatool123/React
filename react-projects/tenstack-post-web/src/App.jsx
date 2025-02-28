83a6536347ccab40c5d8138f9ed7adfed2b48149
SairaBatool123 <sairabatool787@gmail.com>
2025-02-28 23:24:36 +0500

    Done

---
+++ react-projects/tenstack-post-web/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbars from './assets/components/Navbars'
+
+function App() {
+
+  return (
+    <>
+      <Navbars />
+    </>
+  )
+}
+
+export default App
